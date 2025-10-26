ba54eb7f67216265ea3382bb68fafbb5a82e552c
GaziPacarizi <gazmendpaqarizi@gmail.com>
2025-10-26 14:22:53 +0100

    perditesime

--- 06_PROJECTS/Llogaritjet hidraulike te ujesjellesit/JAVASCRIPT/llogaritjet_e_prurjeve.js
+++ 06_PROJECTS/Llogaritjet hidraulike te ujesjellesit/JAVASCRIPT/llogaritjet_e_prurjeve.js
@@ -11,7 +11,7 @@
 
     const Nb = Nb0 * Math.pow(1 + p / 100, n);
     const Qmes = (Nb * qmes) / (24 * 3600); // l/s
-    const Qmaks_d = Qmes * 1.5 * 86.4; // m³/ditë
+    const Qmaks_d = Qmes * 1.5; // l/s
     const Qmaks_o = Qmaks_d * 1.5;
 
     document.getElementById("numri_banoreve_periudhe_projektuese").innerText =
@@ -21,22 +21,8 @@
       "Prurja mesatare ditore është: " + Qmes.toFixed(2) + " l/s";
 
     document.getElementById("prurja_maksimale_ditore").innerText =
-      "Prurja maksimale ditore është: " + Qmaks_d.toFixed(2) + " m³/ditë";
+      "Prurja maksimale ditore është: " + Qmaks_d.toFixed(2) + " l/s";
 
     document.getElementById("prurja_maksimale_orore").innerText =
-      "Prurja maksimale orore është: " + Qmaks_o.toFixed(2) + " m³/orë";
-
-    updateTable(); // përditëso tabelën
-
-    const maxPositive = parseFloat(localStorage.getItem("maxPositive")) || 0;
-    const maxNegative = parseFloat(localStorage.getItem("maxNegative")) || 0;
-
-    const Vrez = (maxPositive + maxNegative) * Qmaks_d /100;
-    const Vzj = 36;
-    const Vshtëse = 0.25 * (Vrez + Vzj);
-    const Vtot = Vrez + Vzj + Vshtëse;
-
-    document.getElementById("rezervuari").innerText = Math.ceil(Vrez.toFixed(2)/5)*5 + " m³";
-    document.getElementById("rezervuari_vtot").innerText =
-      "Vëllimi total i rezervuarit është: " + Vtot.toFixed(2) + " m³";
+      "Prurja maksimale orore është: " + Qmaks_o.toFixed(2) + " l/s";
   };
